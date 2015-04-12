2e36edb12a7c50647d4556779896f648fa4b0084
Theron Boerner <hunterboerner@gmail.com>
2015-04-12 17:23:10 -0500

    That was easy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate toml;
+extern crate rustc_serialize;
+use rustc_serialize::json;
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut string = String::new();
+    let _ = stdin.lock().read_to_string(&mut string);
+    let value = toml::Parser::new(&string).parse().unwrap();
+    let encoded = json::encode(&value).unwrap();
+    println!("{}", encoded);
 }
